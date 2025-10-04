28615ac6a967f8efc9a464e92e0b59447535271b
NKumar1612 <nkumar07.nk@gmail.com>
2025-10-04 12:47:37 +0530

    cleaning the file structure by putting the assests and section folders outside the app folder.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
-import Navigation from "./sections/Navigation";
+import Navigation from "../sections/Navigation";
 
 const opensans = Open_Sans({
   subsets: ["latin"],
